522cecb32b215ea570ce2928a003ac9c4c1e22da
Jake <jwday04@gmail.com>
2024-09-26 12:28:05 +0100

    implemented csv search function

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import tkinter as tk
+import csv
 
 def getinput():
-    try:
-        txt = int(inputtxt.get(1.0, "end-1c"))
-        print(txt)
-        info = lookup(id, 'products.txt')
-        productinfo.config(text=info)
-    except:
-        print('enter valid integer ID')
+    txt = str(inputtxt.get(1.0, "end-1c"))
+    #print(txt)
+    info = lookup(txt, 'PriceList.csv')
+    productinfo.config(state='normal')
+    productinfo.insert(tk.END, info)
+    productinfo.config(state='disable')
 
 def lookup(item, productfile):
-    file = open(productfile)
-    text = file.read()
-    return text
+    with open(productfile, 'r', encoding='utf-8') as file:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            if row[0] == item:
+                print(row)
+                return str(row)+'\n'
+
 
 root=tk.Tk()
 
 root.title('Garmin Product Lookup')
-root.geometry('300x400')
+root.geometry('1280x720')
 
 label = tk.Label(root, text='Enter Product ID:')
 label.pack()
@@ -28,7 +32,8 @@ inputtxt.pack()
 submit = tk.Button(root, text='submit', command=getinput) 
 submit.pack()
 
-productinfo=tk.Label(root, text='')
+productinfo=tk.Text(root, height = 30, width = 200, font=('Helvetica',8))
 productinfo.pack()
+productinfo.config(state=tk.DISABLED)
 
 root.mainloop()
